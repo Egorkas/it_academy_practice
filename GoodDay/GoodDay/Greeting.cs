7651d75b8065b0233b65e521b8a04b06f4dfda30
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-02-15 21:47:24 +0300

    the first ex

---
+++ GoodDay/GoodDay/Greeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodDay
+{
+    class Greeting
+    {
+        public static void Greet()
+        {
+            int thisHour = DateTime.Now.Hour;
+            if (thisHour >= 9 && thisHour < 12) Console.WriteLine("Good morning, guys");
+            else if (thisHour >= 12 && thisHour < 15) Console.WriteLine("Good day, guys");
+            else if (thisHour >= 15 && thisHour < 22) Console.WriteLine("Good evening, guys");
+            else Console.WriteLine("Good night!");
+        }
+    }
+}
