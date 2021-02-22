95af16cd6c7021fa6651da87c9c31a840131aa4b
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-02-22 20:38:13 +0300

    Task1

---
+++ lecture_7/lecture_7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lecture_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = CreateEmptyArr();
+            PrintArr(arr);
+        }
+
+        static void PrintArr(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        static int[] CreateEmptyArr()
+        {
+            int[] emptyArr = new int[0];
+            return emptyArr;
+        }
+    }
+}
