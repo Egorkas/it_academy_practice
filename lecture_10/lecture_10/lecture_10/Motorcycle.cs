597b194eaaa67a982468189c19822133537b9f01
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-03-10 20:11:12 +0300

    Comp has been destroyed! I didn't have time for anything

---
+++ lecture_10/lecture_10/lecture_10/Motorcycle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lecture_10
+{
+    class Motorcycle
+    {
+        private int _identificator;
+        public string model = "MX-1000";
+        public string company = "Minsk";
+        public int year;
+        public int distance;
+
+        public int Identif
+        {
+            get { return _identificator; }
+            private set { _identificator = value; }
+        }
+
+        public Motorcycle()
+        {
+            
+        }
+        
+    }
+}
