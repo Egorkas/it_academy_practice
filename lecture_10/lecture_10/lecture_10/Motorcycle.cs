c5f2fbb3297ce6374e625ffc4beafc96a3b2afd3
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-03-15 01:22:18 +0300

    Lecture 10

--- lecture_10/lecture_10/lecture_10/Motorcycle.cs
+++ lecture_10/lecture_10/lecture_10/Motorcycle.cs
@@ -6,22 +6,128 @@ namespace lecture_10
 {
     class Motorcycle
     {
-        private int _identificator;
-        public string model = "MX-1000";
-        public string company = "Minsk";
-        public int year;
-        public int distance;
+        private Guid _identificator;
+        private string _model = "XM-1000";
+        private string _company = "Minsk";
+        private int _year;
+        private int _distance;
 
-        public int Identif
+        public Guid Identificator
         {
-            get { return _identificator; }
-            private set { _identificator = value; }
+            get
+            {
+                return _identificator;
+            }
+            private set
+            {
+                _identificator = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+        public string Company
+        {
+            get
+            {
+                return _company;
+            }
+            set
+            {
+                _company = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return DateTime.Now.Year;
+            }
+            private set
+            {
+                _year = value;
+            }
+        }
+        public int Distance
+        {
+            get
+            {
+                return _distance;
+            }
+            set
+            {
+                if(value <= 100)
+                _distance = value;
+                else
+                {
+                    Console.WriteLine("Invalid distance! The value must be less or equal 100!!!");
+                }
+            }
+        }
+
+        public void Discharge()
+        {
+            Identificator = Guid.Empty;
+            Model = "XM-1000";
+            Company = "Minsk";
+            Year = 0;
+            Distance = 0;
+            Console.WriteLine("All values became default!!!");
+        }
+                
+        public class Engine
+        {
+            private int _volume;
+            private int _power;
+
+            public int Volume
+            {
+                get
+                {
+                    return _volume;
+                }
+                set
+                {
+                    if(value >= 125 && value <= 3200)
+                    _volume = value;
+                    else
+                    {
+                        Console.WriteLine("Invalid volume! The volume must be more or equal 125 and less or equal 3200!!!");
+                    }
+                }
+            }
+
+            public int Power
+            {
+                get
+                {
+                    return _power;
+                }
+                set
+                {
+                    if(value >= 50 && value <= 300)
+                    _power = value;
+                    else
+                    {
+                        Console.WriteLine("Invalid power! The power must be more or equal 50 and less or equal 300!!!");
+                    }
+                }
+            }
+
         }
 
         public Motorcycle()
         {
-            
+            Identificator = Guid.NewGuid();
         }
-        
+
     }
 }
--- lecture_10/lecture_10/lecture_10/Program.cs
+++ lecture_10/lecture_10/lecture_10/Program.cs
@@ -6,7 +6,42 @@ namespace lecture_10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Moto 1
+            Motorcycle moto1 = new Motorcycle();
+            moto1.Model = "XN-1293";
+            moto1.Company = "Minsk";
+            moto1.Distance = 90;
+            Motorcycle.Engine forMoto1 = new Motorcycle.Engine();
+            forMoto1.Volume = 126;
+            forMoto1.Power = 50;
+
+            //Moto 2
+            Motorcycle moto2 = new Motorcycle();
+            moto2.Model = "Suzuki-200";
+            moto2.Company = "Suzuki";
+            moto2.Distance = 0;
+            Motorcycle.Engine forMoto2 = new Motorcycle.Engine();
+            forMoto2.Volume = 3000;
+            forMoto2.Power = 300;
+
+            moto2.Discharge();
+
+            //Moto 3
+            Motorcycle moto3 = new Motorcycle();
+            moto3.Model = "XRGT-290";
+            moto3.Company = "Kavasaki";
+            moto3.Distance = 20;
+            Motorcycle.Engine forMoto3 = new Motorcycle.Engine();
+            forMoto3.Volume = 3200;
+            forMoto3.Power = 290;
+
+            Motorcycle[] array = { moto1,  moto2,  moto3 };
+            Motorcycle.Engine[] arrMoto = { forMoto1, forMoto2, forMoto3 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. Motorcycle (Company): {array[i].Company}, Model: {array[i].Model}, Identificator: {array[i].Identificator}, Year: {array[i].Year}");
+                Console.WriteLine($"{i+1}.1 Engine(Volume): {arrMoto[i].Volume}, Power: {arrMoto[i].Power}.");
+            }            
         }
     }
 }
