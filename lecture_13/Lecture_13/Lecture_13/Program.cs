136f11c7cc9f8d94110df74978a94026a265bf88
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-03-22 19:33:37 +0300

    PZ_13

---
+++ lecture_13/Lecture_13/Lecture_13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lecture_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uniqueltem.Id = 0;
+            Uniqueltem uniq = new Uniqueltem();
+            Uniqueltem uniq1 = new Uniqueltem();
+            Uniqueltem uniq2 = new Uniqueltem();
+            Uniqueltem uniq3 = new Uniqueltem();
+            Uniqueltem.DisplayId();// ID = 4
+        }
+    }
+}
---
+++ lecture_13/Lecture_13/Lecture_13/Uniqueltem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture_13
+{
+    class Uniqueltem
+    {
+        private static int id;
+
+        public static int Id
+        { 
+            get
+            {
+                return id;
+            } 
+            set 
+            {
+                id = value; 
+            } 
+        }
+        public Uniqueltem()
+        {
+            id++;
+        }
+
+        public static void DisplayId()
+        {
+            Console.WriteLine($"Your Id is - {Id}");
+        }
+    }
+}
